2a12103f474969115e27a630d811222d62819a43
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-26 21:11:54 +0000

    Add the active record callback for this method and make this method private

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
+  after_save :update_posts_counter
+
+  private
 
   def update_likes_counter
     post.increment(:likes_counter)
