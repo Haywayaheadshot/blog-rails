5200c26ace0e9cb43bf408def990ddf8c79d75e7
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-26 20:43:46 +0000

    Add application_record for posts

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+    belongs_to :author, foreign_key: 'author_id', class_name: 'User'
+    has_many :comments, foreign_key: :author_id
+    has_many :likes, foreign_key: :author_id
+    validates :title, length: { maximum: 250 }
+    validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+    validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  
+    def update_posts_counter
+      author.increment(:posts_counter)
+    end
+  
+    def recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+  end
