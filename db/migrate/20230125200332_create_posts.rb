4ac1be71260edac2c7b29ff5d6ebc25de94f72d7
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-27 17:50:20 +0000

    Delete photo

--- db/migrate/20230125200332_create_posts.rb
+++ db/migrate/20230125200332_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :photo
       t.text :text
       t.integer :comments_counter, default: 0
       t.integer :likes_counter, default: 0
