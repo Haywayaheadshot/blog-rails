529d96cd7cf6fefc4758fbc3d6f2b5e7c8194770
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-27 17:50:44 +0000

    Delete private method

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,15 +5,10 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-  after_save :update_posts_counter
-
-  private
 
   def update_posts_counter
     author.increment(:posts_counter)
   end
-
-  public
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
