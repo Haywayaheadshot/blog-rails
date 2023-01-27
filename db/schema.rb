2b788eace06a18838b54b47ad30f3b1b64178f7d
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-27 17:49:12 +0000

    Regenrated schema with default values for counters as 0

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_201449) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "photo"
     t.text "text"
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
