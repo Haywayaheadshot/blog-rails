f0388b073ec8836b54ece3ae0c6855c82f4f46c4
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-01 00:04:45 +0000

    Change root destination and build resources to handle routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'pages#hello'
+  root to: 'user#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :user,only: [:show] do 
+    resources :post, only: [:index, :show]
+  end
 end
