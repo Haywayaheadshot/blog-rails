7b29b52a8bd593263fca716a8855af01185b3b35
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-02 23:42:08 +0000

    Add foreign key

--- db/migrate/20230125201449_create_comments.rb
+++ db/migrate/20230125201449_create_comments.rb
@@ -1,11 +1,12 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, foreign_key: { to_table: 'users' }
-      t.references :posts, foreign_key: { to_table: 'posts' }
+      t.integer :author_id, index: true
+      t.integer :post_id, index: true
       t.text :text
-
       t.timestamps
     end
+    add_foreign_key :comments, :users, column: :author_id
+    add_foreign_key :comments, :posts, column: :post_id
   end
 end
