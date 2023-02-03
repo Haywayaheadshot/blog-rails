e49783f6206cadfdfdfccd63340a258a0770a598
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-03 21:10:56 +0000

    Add resources and routes to handle comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/user/:user_id/post/:user_id', to: 'post#index', as: 'show_posts'
+  get '/user/:user_id/post/:user_id/comments/new', to: 'comments#new', as: 'new_comment'
+  post '/user/:user_id/post/:user_id/comments', to: 'comments#create', as: 'create_comment'
+  post '/user/:user_id/post/:user_id/likes', to: 'likes#create', as: 'create_like'
 
   resources :user,only: [:show] do 
     resources :post, only: [:index, :show]
   end
+
+  resources :post, only: [:new, :create]
 end
