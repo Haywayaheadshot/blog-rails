7c11a9a0774150244283d3b832249e0071727436
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-10 15:49:41 +0000

    Add exclamation mark to increments

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-  after_save :update_posts_counter
+  after_create :update_posts_counter
 
   def update_posts_counter
     author.increment!(:posts_counter)
